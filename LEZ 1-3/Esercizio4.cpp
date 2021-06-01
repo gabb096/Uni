6f72895e7b668d9efb683c69afc164abb2996882
gabb096 <70808154+gabb096@users.noreply.github.com>
2021-06-01 14:22:11 +0200

    Delete LEZ 1-3 directory

--- LEZ 1-3/Esercizio4.cpp
+++
@@ -1,5 +0,0 @@
-/*
-Creare una funzione swap() cui dati in input due puntatori 
-scambia tra di loro il valore delle variabili cui puntano.
-Usare questa funzione all'interno del main() (scegliete voi i numeri di esempio)
-*/
